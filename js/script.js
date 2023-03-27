6da95de0d32fc6e6258079be2e0d3ce68fa197cb
Dmitry <klewzow@gmail.com>
2023-03-27 10:14:50 +0300

    add job chernrnko

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const MAX_PAGE = 6;
-const MIN_PAGE = 1;
-const DEFAULT_PAGE = 1;
 function ibg() {
 
     let ibg = document.querySelectorAll(".ibg");
@@ -15,17 +12,40 @@ ibg();
 
 
 
-const anchors = document.querySelectorAll('slide');
+// const anchors = document.querySelectorAll('slide');
 
-for (var i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener('click', function (e) {
-        e.preventDefault();
+// for (var i = 0; i < anchors.length; i++) {
+//     anchors[i].addEventListener('click', function (e) {
+//         e.preventDefault();
 
-        const blockID = anchor.getAttribute('href');
+//         const blockID = anchor.getAttribute('href');
 
-        document.querySelector(blockID).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    });
-} 
+//         document.querySelector(blockID).scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         });
+//     });
+// } 
+
+/*
+ * Smooth page scroll
+ * плавный скролл по страннице
+ */
+
+// function documentSlideAction(el) {
+//     const targetElement = el.target;
+//     if (targetElement.closest('[data-goto]')) {
+
+//         const goto = targetElement.closest('[data-goto]').dataset.goto;
+//         const gotoElement = document.querySelector(goto);
+//         const headerHeight = document.querySelector('.header').offsetHeight;
+//         if (gotoElement) {
+//             window.scrollTo({
+//                 top: gotoElement.offsetTop - (headerHeight + 60),
+//                 behavior: "smooth"
+//             });
+//         }
+//         el.preventDefault();
+//     }
+// }
+
